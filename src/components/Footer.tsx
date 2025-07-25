64c3171283d03422dd38bf99ac703d3f6f484a9d
francobg7 <francobg7@gmail.com>
2025-07-24 21:20:27 -0300

    year changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -52,7 +52,7 @@ const Footer = () => (
         </div>
       </div>
       <div className="border-t border-gray-800 mt-12 pt-8 text-center">
-        <p className="text-gray-400">&copy; 2024 Black Colors. Todos los derechos reservados.</p>
+        <p className="text-gray-400">&copy; 2025 Black Colors. Todos los derechos reservados.</p>
       </div>
     </div>
   </footer>
